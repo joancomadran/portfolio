797e75c1ae1e06b0c50bbde94c81417e200dffad
Joan Comadran <joan.comadran2@gmail.com>
2023-06-22 17:03:06 +0200

    deleted card

--- app.js
+++ app.js
@@ -4,29 +4,32 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set("views", "views/")
+app.set("view engine", "ejs");
+app.set("views", "views/");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", function(req, res){
-    res.render("home");
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get("/contact", function(req, res){
-    res.render("contact");
-})
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
 
-app.get("/aboutme", function(req, res){
-    res.render("aboutme");
-})
+app.get("/aboutme", function (req, res) {
+  res.render("aboutme");
+});
 
-app.get("/projects", function(req, res) {
-    res.render("projects");
-})
+app.get("/projects", function (req, res) {
+  res.render("projects");
+});
 
-app.listen(3000, function(){
-    console.log("Server running on port 3000");
-})
+app.get("/gamingwebapp", function (req, res) {
+  res.render("gamingwebapp");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server running on port 3000");
+});
